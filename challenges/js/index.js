a8bb27de4611b6bc9ed3839fc8bc7dbc8128c23b
Gary Simmons <gary.simmons@colorado.edu>
2018-12-05 11:07:47 -0500

    add the helloworld c sharp project

--- challenges/js/index.js
+++ challenges/js/index.js
@@ -1,28 +1,69 @@
 //chalenge 2: Given the longest sentence, find the longest word
 
-var sentence=prompt('Please type in a sentence')
-
 /*break sentence up via spaces.
 Place words in array.
 Have function check each word against each other
 Longer word moves on and challenges next word in array
 Winning string is sent to innerHTML*/
 
-// function punctuationDeleter(givenSentence,punctuation){
-//   for(var char=0;char<givenSentence.length;char+=1){
-//     char.replace(punctuation,'');
-//   }
-//   return givenSentence
-// }
+//create white list
 
-// sentence=punctuationDeleter(sentence,'.')
-// //debugger
-// sentence=punctuationDeleter(sentence,'!')
-// sentence=punctuationDeleter(sentence,'?')
+var allowables=[" ","'","-"]
+
+function listAppender(first,last){
+  for(index=first.charCodeAt();index<=last.charCodeAt();index+=1){
+    allowables.push(String.fromCharCode(index))
+  }
+}
+
+listAppender("a","z");
+listAppender("A","Z");
+
+function bouncer(guest){
+  var listLength=allowables.length;
+  var onList=false
+  for (var line=0;line<listLength;line+=1){
+    if (guest==allowables[line]){
+      var onList=true
+      return onList
+    }
+  }
+  return onList
+}
+
+//end white list code
+
+//set up dom
+var button=document.querySelector('button')
+var sentenceElement=document.querySelector('#sentence')
+var div=document.createElement('div')
+document.body.appendChild(div)
 
 
+function longestWord(evt){
+  var givenSentence=sentenceElement.value
+  debugger
+  var unpunctuated=''
+  var numOfChars=givenSentence.length
 
-var words=sentence.split(" ")
+  for (var questionable=0;questionable<numOfChars;questionable+=1){
+    var testValue=givenSentence.charAt(questionable)
+    if(bouncer(testValue)){
+      var unpunctuated=unpunctuated+testValue;
+    }
+  }
+
+  var words=unpunctuated.split(" ")
+
+  var winner=words[0]
+  for (var word=1;word<words.length;word+=1){
+    var winner=wordComparison(winner,words[word])
+  }
+  event.preventDefault()
+  div.innerText=`Your longest word is ${winner}`
+  debugger
+}
+
 
 function wordComparison(a,b){
   /*Compares 2 words against each other
@@ -35,13 +76,5 @@ function wordComparison(a,b){
     return b
   }
 }
-var winner=words[0]
-for (var word=1;word<words.length;word+=1){
-  var winner=wordComparison(winner,words[word])
-}
-console.log(winner)
 
-var div=document.createElement('div')
-div.innerHTML=`The longest word in your sentences is ${winner}`
-
-document.body.appendChild(div)
+button.addEventListener('click',longestWord)
