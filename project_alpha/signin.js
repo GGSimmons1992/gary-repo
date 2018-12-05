7aa7a1316a0dfa502e3efc3b5dd94b7a148826ab
Gary Simmons <gary.simmons@colorado.edu>
2018-12-04 22:04:34 -0500

    update login.html and signin.js

--- project_alpha/signin.js
+++ project_alpha/signin.js
@@ -1,8 +1,11 @@
 //signin.js
 //Description: Logic behind login.html
 
+//login logic
+
+
 //cipher and store passwords
-(function (){
+var encryption=(function (){
   var users=['redEye','blueMoon','blackieChan']
   var plainPasswords=['password123','silence94','firetruck691']
   var cipheredPasswords=[]
@@ -28,26 +31,72 @@
     numbers.push(`${numberIndex}`)
   }
   var uncryptedArray=upperAlphabet.concat(lowerAlphabet).concat(numbers).concat(symbols)
+  //debugger
   var encryptedArray=[]
-  for(var plainIndex=0;plainIndex<uncryptedArray.length;plainIndex=+1){
-    var codedIndex=plainIndex
-    encryptedArray.push(uncryptedArray[codedIndex])
+  var uncryptedArrayLength=uncryptedArray.length
+  for(var plainIndex=0; plainIndex<uncryptedArrayLength; plainIndex++){
+    var codedIndex=(plainIndex+5)%uncryptedArrayLength
+    encryptedArray.unshift(uncryptedArray[codedIndex])
+    //debugger
+  }
+  //debugger
+  //console.log(uncryptedArray)
+
+  function encrypter(character){
+    for (var uncryptedValue=0;uncryptedValue<uncryptedArray.length;uncryptedValue++){
+      if (character==uncryptedArray[uncryptedValue]){
+        return encryptedArray[uncryptedValue]
+      }
+    }
   }
 
-  //console.log(uncryptedArray)
+  function cipher(plainPassword){
+    for (var place=0;place<plainPassword.length;place++){
+      if (place==0){
+        var newPassword=encrypter(plainPassword.charAt(place))
+      }else{
+        newPassword=newPassword+encrypter(plainPassword.charAt(place))
+      }
+    }
+    return newPassword
+  }
 
   //End cipher code
 
-  for (var passwordNumber=0;passwordNumber<plainPasswords.length;passwordNumber=+1){
+  for (var passwordNumber=0;passwordNumber<plainPasswords.length;passwordNumber++){
      var ciphered=cipher(plainPasswords[passwordNumber])
      cipheredPasswords.push(ciphered)
+     debugger
      }
   // }
-   console.log(ciphered)
-   localStorage.setItem("users", users)
-   localStorage.setItem("passwords", cipheredPasswords)
+   sessionStorage.setItem("users", users)
+   sessionStorage.setItem("passwords", cipheredPasswords)
+
+   return {
+     codeUp:cipher
+   }
+
 })()
 
+//console.log(encryption.codeUp('abcde'))
+
+var signInButton=document.querySelector('#signInButton')
+var username=document.querySelector('#username')
+var enteredPassword=document.querySelector('#enteredPassword')
+
+function validator(user,userpassword){
+  /*
+    var userlist=sessionStorage.getItem('users')
+    var passlist=sessionStorage.getItem('passwords')
+    userLength=users.length
+    var userValidation=false
+    for (var userIndex=0;userIndex<userLength;user+=1){
+      if users[]
+    }
+  */
+}
+
+signInButton.addEventListener('click',validator(username,enteredPassword))
 
 
 
