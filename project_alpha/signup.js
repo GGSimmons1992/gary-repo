5a082a5f532d4bbcb6cf656c1538be065fcf1c15
Gary Simmons <gary.simmons@colorado.edu>
2018-12-17 16:57:33 -0500

    edit signup.html and signup.js

--- project_alpha/signup.js
+++ project_alpha/signup.js
@@ -2,6 +2,28 @@
 /*
 Description: Allows users to create a username and password
 */
-var 
+var button=document.querySelector("button");
+button.addEventListener("click",validate);
+
+
 //--function line--
 
+function validate(){
+  var enteredUser=document.querySelector('#username');
+  var enteredPass1=document.querySelector('#password1');
+  var enteredPass2=document.querySelector('#password2');
+
+  if (enteredPass1===enteredPass2){
+    //Begin to post
+    var url="https://localhost:5001/api/user";
+    var request=new Request(url,{
+      method:post,
+      body:{username:username,password:password},
+      headers:{
+        'content-type':'application/json'
+      }
+    });
+  }else{
+    //div passwords do not match
+  }
+}
