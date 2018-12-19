e2fd700c46ee3c757496ee3589300fd88a03e91c
Gary Simmons <gary.simmons@colorado.edu>
2018-12-19 16:41:53 -0500

    start SelectionChallenge

---
+++ challenges/SelectionChallenge/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SelectionChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          String[] myArray=new String[]{};
+          System.Console.WriteLine(myArray);
+        }
+
+        static String[] PlayWithArrays(String[] someArray)
+        {
+          //Put numbers in before letters.
+
+          String[] lowerArray=someArray;
+          String[] categoryArray=new String[36];
+
+          int lowerArrayIndex=0;
+          foreach (var item in someArray)
+          {
+            lowerArray=item.toLower();
+          }
+
+          for(index=0;index<10;index+=1)
+          {
+            categoryArray[index]=index;
+          }
+          for(index=10;index<someArray.Length;)
+          {
+            
+          }
+
+
+          return newArray;
+        }
+    }
+}
