ba21711c0a1eeffd7d0582a4ea5d4c6928862c3e
GGSimmions1992 <gary.simmons@colorado.edu>
2018-12-24 18:40:04 -0500

    transfer Program.cs of SelectionChallenge to SelectionWorld2

---
+++ challenges/SelectionWorld2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SelectionWorld2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var inputArray=new string[]{"vasni","asvnsaip","nasvipsn"};
+          var myArray=PlayWithArrays(inputArray);
+          System.Console.WriteLine(myArray);
+        }
+
+        private static String[] PlayWithArrays(String[] someArray)
+        {
+          //Put numbers in before letters.
+          String[] finalArray=null;
+          String[] lowerArray=someArray;
+          
+          foreach (var item in someArray)
+          {
+            item.ToLower();
+          }
+          
+          for(var i=1;i<someArray.Length;i++)
+          {
+            
+          }
+
+          return finalArray;
+        }
+    }
+}
